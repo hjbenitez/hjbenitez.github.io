f5b6b03e115b163deea92bc5038fe86d856a1fda
hjbenitez <humbertobenitez5760@yahoo.ca>
2024-03-28 21:38:56 -0400

    footer work: the sequel

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  function changeColour(
+    primaryColour, primaryColourShade,
+    secondaryColour, secondaryColourShade,
+    tertiaryColour, tertiaryColourShade) {
+
+    document.querySelector(
+      ":root").style.cssText = 
+      `
+      --primary-colour:` +  primaryColour + `;
+      --primary-colour-shade:` +  primaryColourShade + `;
+
+      --secondary-colour:`+ secondaryColour +`;
+      --secondary-colour-shade:`+ secondaryColourShade +`;
+
+      --tertiary-colour:`+ tertiaryColour +`;
+      --tertiary-colour-shade:`+ tertiaryColourShade +`;
+      `;
+  }
